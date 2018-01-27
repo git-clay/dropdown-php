b60c36ff3681e3701e4b964738a0a1e235cf4825
git-clay <ce.smith5525@gmail.com>
2018-01-27 10:06:01 -0700

    reset

--- Untitled-1.js
+++ Untitled-1.js
@@ -45,104 +45,60 @@ $(document).ready(function() {
   }else{
     $('#create-wo-m').hide()
   }
-  /**
-   * *Function Section * *
-   */
-    //returns one workout based on focus exercise
-    //called on level/type selection, refresh, add
-    /**
-     * getwo is the main call
-     * ? maybe a promise instead of callback
-     * @param focusExer 
-     * @param callback
-     * * public method refresh
-     * * public method add
-     */
-    function getWo(focusExer,callback) {
-      var queryAction = "workout_ajax";
-      this.workoutObj = {
-        "level":"",
-        "type":"",
-        "focus":focusExer.toLowerCase()
-      }
-      var ajaxurl = "http://fitnessproblem.com/wp-admin/admin-ajax.php"
-      var data = {
-        'action': queryAction,
-        'name': workoutObj
-      };
-      console.log(this.workoutObj)
-      function request(){
-        jQuery.ajax({
-        url: ajaxurl,
-        type: "POST",
-        data: data,
-        success: function(res) {
-          if(res==null || res == ''){
-            getWo(focusExer,callback)
-          }else{
-            callback(res)
-          }
+
+  function getWo(focusExer,callback) {
+    var queryAction = "workout_ajax";
+    var workoutObj = {"level":level.toLowerCase(),"type":type.toLowerCase(),"focus":focusExer.toLowerCase()} //build query serverside
+    var ajaxurl = "http://fitnessproblem.com/wp-admin/admin-ajax.php"
+    var data = {
+      'action': queryAction,
+      'name': workoutObj
+    };
+    jQuery.ajax({
+      url: ajaxurl,
+      type: "POST",
+      data: data,
+      success: function(res) {
+        if(res==null || res == ''){
+          getWo(focusExer,callback)
+        }else{
+          callback(res)
         }
-        })
       }
-      this.add = function(){
-      console.log('add')
-      }
-      this.refresh = function(){
+    })
+  }
 
-      }
+  //refresh "constructor"
+  function refresh(tempFocus) {
+    if(tempFocus == 'resistance' && resEdit){
+      updateResOption($('#resistance-selected').text(),'#resistanceMenu')
+    }else if(tempFocus == 'plyo'&&plyoEdit){
+      updateResOption(plyoVal,'#plyoMenu')
+    }else if(tempFocus == 'saqBtn' && saqEdit){
+      updateResOption(saqVal,'#saqMenu')
+    }else if(tempFocus == 'coreBtn' && coreEdit){
+      updateResOption(coreVal,'#coreMenu')
+    }else{
+      var content = '#' + tempFocus + '-content'
+      $(content).text("Loading...")
+        getWo(tempFocus,function(newWorkout){
+          $(content).text(newWorkout)
+        });
     }
+  }
 
-    //refresh "constructor"
-    //todo  combine with getWo as a public method?
-    function refresh(tempFocus) {
-      if(tempFocus == 'resistance' && resEdit){
-        updateResOption($('#resistance-selected').text(),'#resistanceMenu')
-      }else if(tempFocus == 'plyo'&&plyoEdit){
-        updateResOption(plyoVal,'#plyoMenu')
-      }else if(tempFocus == 'saqBtn' && saqEdit){
-        updateResOption(saqVal,'#saqMenu')
-      }else if(tempFocus == 'coreBtn' && coreEdit){
-        updateResOption(coreVal,'#coreMenu')
-      }else{
-        var content = '#' + tempFocus + '-content'
-        $(content).text("Loading...")
-          getWo(tempFocus,function(newWorkout){
-            $(content).text(newWorkout)
-          });
-      }
-      function addTr(newWorkout, toRefresh, optionalCls) {
-        var delBtn = ''
-        if(toRefresh != 'warmup' && toRefresh != 'cooldown'){
-          delBtn = "<a class='btn btn-danger delete pull-right' href='#' onclick='rowDelete(this); return false;'>X</a>"
-        }
-        if(newWorkout == '' || newWorkout =="Loading..."){return false}
-        $('#' + toRefresh + ' tr:last').after('<tr class="' + optionalCls + '"><td>'+ newWorkout + delBtn + "</td></tr>")
-      }
+  //adding a row to the correct results table to the right with the given workout from getWo
+    //can maybe combine with getWo as a private method
+  function addTr(newWorkout, toRefresh, optionalCls) {
+    var delBtn = ''
+    if(toRefresh != 'warmup' && toRefresh != 'cooldown'){
+      delBtn = "<a class='btn btn-danger delete pull-right' href='#' onclick='rowDelete(this); return false;'>X</a>"
     }
-    //adding a row to the correct results table to the right with the given workout from getWo
-    //todo might be refresh's child method
-    function addTr(newWorkout, toRefresh, optionalCls) {
-      var delBtn = ''
-      if(toRefresh != 'warmup' && toRefresh != 'cooldown'){
-        delBtn = "<a class='btn btn-danger delete pull-right' href='#' onclick='rowDelete(this); return false;'>X</a>"
-      }
-      if(newWorkout == '' || newWorkout =="Loading..."){return false}
-      $('#' + toRefresh + ' tr:last').after('<tr class="' + optionalCls + '"><td>'+ newWorkout + delBtn + "</td></tr>")
-    }
+    if(newWorkout == '' || newWorkout =="Loading..."){return false}
+    $('#' + toRefresh + ' tr:last').after('<tr class="' + optionalCls + '"><td>'+ newWorkout + delBtn + "</td></tr>")
+  }
 
-    /* Resistance Dropdown */
-    //todo combine into refresh
-    function updateResOption(focus, thisMenu) {
-      $(thisMenu).text("Loading...")
-      getWo(focus,function(newWorkout){
-        $(thisMenu).text(newWorkout + ' ').append('<span class="caret"></span>')
-      });
-    }
-  /**
-   * * Two click handlers for main dropdowns (wo, focus)
-   * TODO: make a parent "class" for click handlers
-   */
+
   //click - main dropdown select for experience and type and level
   $('.wo').on("click", function(e) {
     resEdit=false;coreEdit=false;saqEdit=false;plyoEdit=false;
@@ -217,91 +173,17 @@ $(document).ready(function() {
 
     if (expSelected) {$('.expbased').remove()}
     expSelected = true;
-  
-    // getWo("warmup",function(woArray){
-    //   woArray = JSON.parse(woArray)
-    //   $.each(woArray,function(i,exer){
-    //     addTr(exer, "warmup", "expbased")
-    //     addTr(exer, "cooldown", "expbased")
-    //   })
-    // });
 
-    getWo("core",function(woArray){
-      // woArray = JSON.parse(woArray)
-      // $.each(woArray,function(i,exer){
-        addTr(woArray, "core", "expbased")
-        // addTr(exer, "core", "expbased")
-      // })
+    getWo("warmup",function(woArray){
+      woArray = JSON.parse(woArray)
+      $.each(woArray,function(i,exer){
+        addTr(exer, "warmup", "expbased")
+        addTr(exer, "cooldown", "expbased")
+      })
     });
-    getWo.workoutObj.level = level
-    getWo.workoutObj.type = type
-    console.log(getWo.workoutObj)
-
     refresh('balance');
   })
-
-  /**
-   * Focus dropdowns
-   * ! make these one class that is applied to all four dropdown options with
-   * @param dropdownTitle core, saq, plyo, resistance
-   * */
-  //click - core dropdown 
-  $('.coreDropdown').on("click", function(e) {
-    coreEdit=true;
-    var tagname;
-    coreVal = $(this).val()
-    if($(this).parent().parent().parent().children()[0].text == undefined){
-      tagname = $(this).text()
-    }else{
-      tagname = $(this).parent().parent().parent().children()[0].text + ' - ' + $(this).text()
-    }
-    updateResOption(coreVal,'#coreMenu')
-    $('#core-selected').text(tagname)
-  })
-  //click - saq dropdown
-  $('.saqDropdown').on("click", function(e) {
-    saqEdit=true;
-    var tagname;
-    saqVal = $(this).val()
-    if($(this).parent().parent().parent().children()[0].text == undefined){
-      tagname = $(this).text()
-    }else{
-      tagname = $(this).parent().parent().parent().children()[0].text + ' - ' + $(this).text()
-    }
-    updateResOption(saqVal,'#saqMenu')
-    $('#saq-selected').text(tagname)
-  })
-  //click - plyo dropdown 
-  $('.plyoDropdown').on("click", function(e) {
-    plyoEdit=true;
-    var tagname;
-    plyoVal= $(this).val()
-    if($(this).parent().parent().parent().children()[0].text == undefined){
-      tagname = $(this).text()
-    }else{
-      tagname = $(this).parent().parent().parent().children()[0].text + ' - ' + $(this).text()
-    }
-    updateResOption(plyoVal,'#plyoMenu')
-    $('#plyo-selected').text(tagname)
-  })
-  //click - resistance dropdown //? rename to resistanceDropdown
-  $('.resSelection').on("click", function(e) {
-    var tagname;
-    resVal = $(this).val()
-    if($(this).parent().parent().parent().children()[0].text == undefined){
-      tagname = $(this).text()
-    }else{
-      tagname = $(this).parent().parent().parent().children()[0].text + ' - ' + $(this).text()
-    }
-    updateResOption(resVal,'#resistanceMenu')
-    $('#resistance-selected').text(tagname)
-    resEdit = true;
-  })  
-
-
-
   //click - refresh btn for dropdowns or non-dropdown rows
-  //todo make both a refresh btn with a way to toggle 'isAdd' bool
   $('.refresh').on("click", function(e) {
     refresh(this.id.slice(0, -2))
   })
@@ -328,6 +210,74 @@ $(document).ready(function() {
     refresh(tempFocus)
   })
 
+  //click - core dropdown 
+  $('.coreDropdown').on("click", function(e) {
+    coreEdit=true;
+    var tagname;
+    coreVal = $(this).val()
+    if($(this).parent().parent().parent().children()[0].text == undefined){
+      tagname = $(this).text()
+    }else{
+      tagname = $(this).parent().parent().parent().children()[0].text + ' - ' + $(this).text()
+    }
+    updateResOption(coreVal,'#coreMenu')
+    $('#core-selected').text(tagname)
+  })
 
+  //click - saq dropdown
+  $('.saqDropdown').on("click", function(e) {
+    saqEdit=true;
+    var tagname;
+    saqVal = $(this).val()
+    if($(this).parent().parent().parent().children()[0].text == undefined){
+      tagname = $(this).text()
+    }else{
+      tagname = $(this).parent().parent().parent().children()[0].text + ' - ' + $(this).text()
+    }
+    updateResOption(saqVal,'#saqMenu')
+    $('#saq-selected').text(tagname)
+  })
+
+  //click - plyo dropdown 
+  $('.plyoDropdown').on("click", function(e) {
+    plyoEdit=true;
+    var tagname;
+    plyoVal= $(this).val()
+    if($(this).parent().parent().parent().children()[0].text == undefined){
+      tagname = $(this).text()
+    }else{
+      tagname = $(this).parent().parent().parent().children()[0].text + ' - ' + $(this).text()
+    }
+    updateResOption(plyoVal,'#plyoMenu')
+    $('#plyo-selected').text(tagname)
+  })
+  //click -resistance dropdown
+  $('.resSelection').on("click", function(e) {
+    var tagname;
+    resVal = $(this).val()
+    if($(this).parent().parent().parent().children()[0].text == undefined){
+      tagname = $(this).text()
+    }else{
+      tagname = $(this).parent().parent().parent().children()[0].text + ' - ' + $(this).text()
+    }
+    updateResOption(resVal,'#resistanceMenu')
+    $('#resistance-selected').text(tagname)
+    resEdit = true;
+  })  
+
+  /* Resistance Dropdown */
+  function updateResOption(focus, thisMenu) {
+    $(thisMenu).text("Loading...")
+    getWo(focus,function(newWorkout){
+      $(thisMenu).text(newWorkout + ' ').append('<span class="caret"></span>')
+    });
+  }
+
+  //add
+  // $('.addRes').on("click", function(e) {
+  //   if (resEdit) {
+  //     addTr($('#resistanceMenu').text(), "workout-table-resistance", "")
+  //   }
+  // })
 
 });
